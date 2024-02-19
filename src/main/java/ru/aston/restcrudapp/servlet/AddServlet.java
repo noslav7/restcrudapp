cecefde838272d5bbecd5bd4923d8bffbd2d1703
noslav7 <noslav7@gmail.com>
2024-02-19 08:00:13 +0500

    AddServlet fix

--- src/main/java/ru/aston/restcrudapp/servlet/AddServlet.java
+++ src/main/java/ru/aston/restcrudapp/servlet/AddServlet.java
@@ -1,66 +1,40 @@
 package ru.aston.restcrudapp.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Serial;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import ru.aston.restcrudapp.entity.Employee;
+import ru.aston.restcrudapp.service.EmployeeService;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Serial;
 
-@WebServlet("/addition")
+@WebServlet ("/addition")
 public class AddServlet extends HttpServlet {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private void processRequest(HttpServletRequest request, HttpServletResponse response) {
-        response.setContentType("text/html;charset=UTF-8");
-    }
-
-    @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        processRequest(request, response);
+            throws ServletException, IOException {
+        response.setContentType("text/html");
 
-        try {
-            String stringId = request.getParameter("id");
-            int id = Integer.parseInt(stringId);
-            String name = request.getParameter("name");
-            String department = request.getParameter("department");
+        String name = request.getParameter("name");
+        String department = request.getParameter("department");
+        Employee employee = new Employee();
+        employee.setName(name);
+        employee.setDepartment(department);
+        int status = EmployeeService.save(employee);
 
-            Class.forName("org.postgresql.Driver");
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/restcrudapp",
-                    "postgres",
-                    "password");
-
-            Statement statement = connection.createStatement();
-            statement.executeUpdate("insert into employees values (" +
-                    "" + id + ",'" + name + "', '" + department + "')");
-
-            PrintWriter out = response.getWriter();
-            out.println("<h1>Record Inserted Successfully</h1>");
-            String sql = "select * from employees";
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            out.println("<form action = 'employees' method='post'>");
-            out.print("<tr><a href ='employees'>View Employee</a></td></tr>");
-            out.println("</tr>");
-            out.println("</table>");
-            out.println("</form>");
-
-            resultSet.close();
-            statement.close();
-            connection.close();
-        } catch (SQLException sqlException) {
-            throw new RuntimeException("Can't connect the database!", sqlException);
-        } catch (ClassNotFoundException classNotFoundException) {
-            throw new RuntimeException(classNotFoundException);
+        PrintWriter out = response.getWriter();
+        if (status > 0) {
+            out.print("<p>Record saved successfully!</p>");
+            request.getRequestDispatcher("index.html").include(request, response);
+        } else {
+            out.println("Unable to save record!");
         }
+        out.close();
     }
 }
